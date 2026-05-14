82c7e6d1977db612fe834c2a39b2653941052d81
Priyanka Sindhu <priyankasindhu42@gmail.com>
2026-05-14 20:26:12 +0530

    update index.js frontend url update

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ app.use(cors({
     "http://localhost:5173",
     "https://liberary-management-frontend.vercel.app"
   ],
+
+  
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
   allowedHeaders: ["Content-Type", "Authorization"],  
   credentials: true
